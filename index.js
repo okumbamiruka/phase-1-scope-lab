be12ba9aa248a3b2a1e8096b49dd7a9c63ecae10
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-21 19:57:32 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob'; //you can declare a global variable by attaching it to the window object
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'jessica';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'john';
+    console.log(leastFavoriteCustomer)
+}
+
+console.log(leastFavoriteCustomer)
+
+changeLeastFavoriteCustomer()
